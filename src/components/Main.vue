e8a3707817f7cf69876e824fbed652304db97194
davidemontagna <davidemontagna92@gmail.com>
2022-02-02 16:11:51 +0100

    api e inserimento in array dei film fatto

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,17 +1,53 @@
 <template>
     <main>
-        <search-bar />
+        <search-bar @searching="startSearch"/>
         main
     </main>
 </template>
 
 <script>
+import axios from 'axios';
 import SearchBar from './commons/SearchBar.vue';
 
 export default {
     name: 'Main',
     components: {
         SearchBar,
+    },
+
+    data(){
+        return {
+            apiURL: "https://api.themoviedb.org/3/search/movie/",
+            api: "73c18d57cad03bbd2fde6c0db5157c10",
+            arrayMovies: [],
+            movieSearched: ""
+        }
+    },
+
+    
+
+    methods: {
+        getMovie() {
+            axios
+                .get(this.apiURL, {
+                    params: {
+                        api_key: this.api,
+                        query: this.movieSearched
+                    }
+                })
+                .then( (response) => {
+                    this.arrayMovies = response.data.results
+                    console.log(this.arrayMovies)
+                })
+                .catch(function (error) {
+                    console.log(error);
+                })
+        },
+
+        startSearch(search){
+            this.movieSearched = search;
+            this.getMovie()            
+        }
     }
 }
 </script>
--- src/components/commons/SearchBar.vue
+++ src/components/commons/SearchBar.vue
@@ -1,13 +1,25 @@
 <template>
     <div>
-        <input type="text">
-        <button>Search</button>
+        <input type="text" v-model="search">
+
+        <button
+        value="go-search"
+        @click="$emit('searching', search)"
+        >        
+        Search
+        </button>
+
     </div>
 </template>
 
 <script>
 export default {
     name: 'SearchBar',
+    data(){
+        return {
+            search: "",
+        }
+    }
 }
 </script>
 
