39034de885f8f063a53b79fca9dd3f54e2e4bb45
davidemontagna <davidemontagna92@gmail.com>
2022-02-02 14:47:25 +0100

    inserimento search bar e button

---
+++ src/components/Main.vue
@@ -0,0 +1,21 @@
+<template>
+    <main>
+        <search-bar />
+        main
+    </main>
+</template>
+
+<script>
+import SearchBar from './commons/SearchBar.vue';
+
+export default {
+    name: 'Main',
+    components: {
+        SearchBar,
+    }
+}
+</script>
+
+<style>
+
+</style>
